39e2992d5f21a80f365a28de9435bfae0ec9f43d
Betul Yaman <betul.iyican@icredible.com>
2025-06-11 16:39:01 +0300

    Check if there is any policy definition in the path or parent path of the processed file.

--- Guard/authorization_control.c
+++ Guard/authorization_control.c
@@ -1,9 +1,12 @@
 #include "authorization_control.h"
 
+#include "adaptive_radix_tree.h"
 #include "global_context.h"
-#include "policy_engine.h"
+
+#include <string.h>
 
 BOOLEAN control_path_access_right(PFLT_CALLBACK_DATA data, UINT32 access_right);
+BOOLEAN find_prefixes_in_policies(PUNICODE_STRING path);
 
 BOOLEAN is_authorized(_In_ PFLT_CALLBACK_DATA data) {
     PFLT_FILE_NAME_INFORMATION name_info;
@@ -18,14 +21,13 @@ BOOLEAN is_authorized(_In_ PFLT_CALLBACK_DATA data) {
         return FALSE;
     }
 
-    TRIE_NODE* result = NULL;
-    BOOLEAN is_exist = trie_search(g_trie_root, &name_info->Name, (UINT8)name_info->Name.Length, &result);
-    if (!is_exist) {
-        FltReleaseFileNameInformation(name_info);
+    BOOLEAN is_exist_in_policies = find_prefixes_in_policies(&name_info->Name);
+    FltReleaseFileNameInformation(name_info);
+    if (!is_exist_in_policies) {
         return TRUE;
     }
 
-    return control_path_access_right(data, result->access_rights);
+    return control_path_access_right(data, g_art_root->access_rights);
 }
 
 BOOLEAN control_path_access_right(PFLT_CALLBACK_DATA data, UINT32 access_right) {
@@ -43,6 +45,40 @@ BOOLEAN control_path_access_right(PFLT_CALLBACK_DATA data, UINT32 access_right) 
 
     if (access_right & MASK_FULL_ACCESS) {
         return TRUE;
+    }
+
+    return FALSE;
+}
+
+BOOLEAN find_prefixes_in_policies(PUNICODE_STRING path) {
+    if (path == NULL || path->Length == 0 || path->Buffer == NULL) {
+        return FALSE;
+    }
+
+    UNICODE_STRING prefix;
+    prefix = *path;
+    while (prefix.Length > 0) {
+        // Search the current prefix in ART
+        BOOLEAN is_found = art_search(g_art_root, &prefix);
+        if (is_found) {
+            return TRUE; // Found a prefix in policies
+        }
+
+        // Find the position of the last backslash before the current end
+        USHORT i;
+        for (i = prefix.Length / sizeof(WCHAR); i > 0; i--) {
+            if (prefix.Buffer[i - 1] == L'\\') {
+                // Set the new prefix length (excluding trailing part)
+                prefix.Length = (i - 1) * sizeof(WCHAR);
+                prefix.MaximumLength = prefix.Length;
+                break;
+            }
+        }
+
+        // If no backslash found, we've reached the topmost prefix
+        if (i == 0) {
+            break;
+        }
     }
 
     return FALSE;
